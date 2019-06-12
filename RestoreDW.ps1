0fe990b1fa39d04f25d15d330584194345630110
Koos van Strien <koos@bitrainer.nl>
2019-06-12 19:31:08 +0000

    Added PQ and XML samples

--- RestoreDW.ps1
+++
@@ -1,9 +0,0 @@
-cd c:\repos\ssis-training\backups
-
-del AdventureWorksDW2016_empty.bak
-
-git pull
-
-(sqlcmd -S . -Q "ALTER DATABASE [AdventureworksDW2016_empty] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-RESTORE DATABASE [AdventureworksDW2016_empty] FROM  DISK = N'C:\Repos\ssis-training\backups\AdventureWorksDW2016_empty.bak' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5
-ALTER DATABASE [AdventureworksDW2016_empty] SET MULTI_USER")
